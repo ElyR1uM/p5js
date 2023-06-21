96c2c8da641db81a917eb2486d2a4b0794096a09
elpep <mailyrium@gmail.com>
2023-06-21 17:04:52 +0200

    Working on adding IntelliSense compat for Jetbrains WS

--- Game/sketch.js
+++ Game/sketch.js
@@ -1,32 +1,36 @@
-let playerSprite;
-let playerX, playerY;
+/* global p5 */
+import("../Lib/p5.js") // Import p5 library
 
+let playerSprite
+let playerX, playerY
 function preload() {
-  playerSprite = loadImage('./Assets/playerSprite.png')
+  playerSprite = loadImage("./Assets/playerSprite.png")
 }
 
 function setup() {
   playerX = 0;
   playerY = 0;
-  createCanvas(400, 400);
+  createCanvas(400, 400)
 }
 
 function draw() {
-  background(220);
+  background(220)
   image(playerSprite, playerX, playerY)
+  myInput()
 }
 
 function myInput() {
-  if (keyIsDown(a)) {
-    playerX -= 5;
+  if (keyIsDown()) {
+    console.log("a")
+    playerX -= 5
   }
-  if (keyIsDown(d)) {
-    playerX += 5;
+  if (keyIsDown()) {
+    playerX += 5
   }
-  if (keyIsDown(w)) {
-    playerY += 5;
+  if (keyIsDown()) {
+    playerY += 5
   }
-  if (keyIsDown(s)) {
-    playerY -= 5;
+  if (keyIsDown()) {
+    playerY -= 5
   }
 }
