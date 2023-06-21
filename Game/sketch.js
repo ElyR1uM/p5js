a0ec214ce097f299fa095537feb92fed51da8f3b
ElyR1uM <89025797+ElyR1uM@users.noreply.github.com>
2023-06-21 10:08:59 +0000

    First setups for movement

--- Game/sketch.js
+++ Game/sketch.js
@@ -1,10 +1,32 @@
-let playerImage;
+let playerSprite;
+let playerX, playerY;
+
+function preload() {
+  playerSprite = loadImage('./Assets/playerSprite.png')
+}
+
 function setup() {
+  playerX = 0;
+  playerY = 0;
   createCanvas(400, 400);
-  playerImage = loadImage("./Assets/playerSprite.png")
 }
 
 function draw() {
-  image(playerImage, 0, 0)
   background(220);
+  image(playerSprite, playerX, playerY)
+}
+
+function myInput() {
+  if (keyIsDown(a)) {
+    playerX -= 5;
+  }
+  if (keyIsDown(d)) {
+    playerX += 5;
+  }
+  if (keyIsDown(w)) {
+    playerY += 5;
+  }
+  if (keyIsDown(s)) {
+    playerY -= 5;
+  }
 }
